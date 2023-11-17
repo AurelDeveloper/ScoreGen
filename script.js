279d3f45856884571844ae1641184a7c67521e9c
AurelDeveloper <marlon.profils@gmail.com>
2023-11-17 15:23:42 +0100

    debug

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 // Definiere ein leeres Array, um die eingegebenen Namen, Punktzahlen und maximalen Punktzahlen zu speichern
 let scoreTable = [];
-var localStorage = window.localStorage;
-var localStorage = macos.localStorage;
-localStorage.setItem(scoreTable);
-
-let scoreTableLocalStorage = localStorage.getItem(scoreTable);
 
 // Hole die Tabelle aus dem HTML-Dokument
 let table = document.getElementById("scoreTable");
